99d1586d80b10975923fed6d0afe9a21f930861b
https://anansc-admin@bitbucket.org <ana.dias@distrito.me>
2022-12-13 02:04:09 -0300

    validations on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :gender, presence: true
+    validates :email, presence: true
+    validates :birthday, presence: true
+    validates :cpf, presence: true
 end
