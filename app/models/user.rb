95b3fb8f8801eaed3d26fe0c3ee9fc4bbff8c49d
https://anansc-admin@bitbucket.org <ana.dias@distrito.me>
2022-12-13 03:13:43 -0300

    commenting validates to test user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :gender, presence: true
-    validates :email, presence: true
-    validates :birthday, presence: true
-    validates :cpf, presence: true
+    # validates :first_name, presence: true
+    # validates :last_name, presence: true
+    # validates :gender, presence: true
+    # validates :birthday, presence: true
+    # validates :cpf, presence: true
 end
