68d43cc455f851782656c8dfc478b53bfbb44e77
https://anansc-admin@bitbucket.org <ana.dias@distrito.me>
2022-12-13 02:03:56 -0300

    validations on property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
 class Property < ApplicationRecord
+    validates :rooms, presence: true
+    validates :bathroom, presence: true
+    validates :size, presence: true
+    validates :furnished, presence: true
+    validates :sell_price, presence: true
+    validates :rent_price, presence: true
+    validates :state, presence: true
+    validates :city, presence: true
+    validates :bairro, presence: true
+    validates :cep, presence: true
+    validates :number, presence: true
 end
